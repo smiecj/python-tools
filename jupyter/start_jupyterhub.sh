d07050454bc45840def2d591f81f38da48077d6f
smiecj <459559238@qq.com>
2022-01-25 03:53:15 +0000

    fix-jupyter: fix execute current path(scripts path) env.sh failed: use dirname to get current path

--- jupyter/start_jupyterhub.sh
+++ jupyter/start_jupyterhub.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
-. ./env.sh
+script_full_path=$(realpath $0)
+script_parent_path=$(dirname $script_full_path)
+
+. $script_parent_path/env.sh
 
 nohup node $NODE_HOME/bin/configurable-http-proxy --ip 0.0.0.0 --port 8000 --api-ip 127.0.0.1 --api-port $proxy_port --error-target http://127.0.0.1:$proxy_port/hub/error > /dev/null 2>&1 &
 
 conda run -n $conda_env_name_python3 nohup jupyterhub --config $jupyter_home/config/jupyterhub_config.py > /dev/null 2>&1 &
+
+popd
