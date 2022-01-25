b8073c19902675fc6543a116084f0587ca133dd0
smiecj <459559238@qq.com>
2022-01-25 06:47:48 +0000

    fix-jupyter: fix cannot start jupyter in dockerfile: start script add source profile

--- jupyter/stop_jupyterhub.sh
+++ jupyter/stop_jupyterhub.sh
@@ -2,6 +2,8 @@
 
 ps -ef | grep 'jupyterhub_config' | grep -v grep | grep -v "stop_jupyter" | awk '{print $2}' | xargs --no-run-if-empty kill -9
 
+ps -ef | grep 'jupyterhub_idle_culler' | grep -v grep | grep -v "stop_jupyter" | awk '{print $2}' | xargs --no-run-if-empty kill -9
+
 ps -ef | grep 'jupyterhub-singleuser' | grep -v grep | grep -v "stop_jupyter" | awk '{print $2}' | xargs --no-run-if-empty kill -9
 
 ps -ef | grep 'configurable-http-proxy' | grep -v grep | awk '{print $2}' | xargs --no-run-if-empty kill -9
