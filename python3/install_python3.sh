d6a73b2d270dd7051a04a1101c5f4dbbe52a6e19
smiecj <459559238@qq.com>
2022-02-18 16:52:48 +0800

    fix-python3: ll change to ls -l

--- python3/install_python3.sh
+++ python3/install_python3.sh
@@ -13,5 +13,5 @@ yum -y install python3
 ## add python3 to environment
 echo -e "\n# python" >> /etc/profile
 echo "export $python3_env_key_home=/usr" >> /etc/profile
-python3_lib_path=/usr/local/lib/`ll /usr/local/lib | grep python3 | sed 's/.* //g'`
+python3_lib_path=/usr/local/lib/`ls -l /usr/local/lib | grep python3 | sed 's/.* //g'`
 echo "export $python3_lib_key_home=$python3_lib_path/site-packages" >> /etc/profile
