2a3b964efc32c52923b9cce6d06c9c72d3b25905
smiecj <459559238@qq.com>
2022-02-21 15:14:19 +0800

    feature-jupyter: jupyterlab add lsp for autocomplete and error check

--- jupyter/install_jupyterlab.sh
+++ jupyter/install_jupyterlab.sh
@@ -18,4 +18,17 @@ sed -i "s/$jupyter_app_env_key=.*\"/$jupyter_app_env_key=$jupyter_app_lab\"/g" $
 systemctl daemon-reload
 popd
 
+## 安装 jupyterlab lsp
+### https://github.com/jupyter-lsp/jupyterlab-lsp
+### https://github.com/jupyterlab/jupyterlab/issues/2972
+pip_bin=$PYTHON3_HOME/bin/pip3
+jupyter labextension install @krassowski/jupyterlab-lsp 
+$pip_bin install jupyterlab-lsp
+$pip_bin install jupyter-lsp
+$pip_bin install git+https://github.com/krassowski/python-language-server.git@main
+jupyter server extension enable --user --py jupyter_lsp
+
+### continuousHinting 自动补全 需要手动打开
+### Settings -> Advanced Settings Editor -> Code Completion -> set continuousHinting = true
+
 popd
